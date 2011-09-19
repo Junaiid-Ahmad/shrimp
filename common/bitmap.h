8d442ba696bcebb567598b001d3e02b3f8337541
Marta Girdea <marta.girdea@gmail.com>
2011-09-19 13:57:34 -0400

    functions for manupulating larger bitmaps

--- common/bitmap.h
+++ common/bitmap.h
@@ -17,7 +17,7 @@ typedef uint64_t bitmap_type;
 static inline void bitmap_insert_clean(bitmap_type * a, uint bits_per_field, uint index, uint value);
 static inline void bitmap_clear(bitmap_type * a, uint bits_per_field, uint index);
 static inline void bitmap_prepend(bitmap_type * a, uint bits_per_field, uint value);
-static inline uint bitmap_extract(bitmap_type * a, uint bits_per_field, uint index);
+static inline uint bitmap_extract(const bitmap_type * a, uint bits_per_field, uint index);
 
 static inline uint bitmap32_extract(uint32_t * a, uint bits_per_field, uint index);
 
@@ -67,7 +67,7 @@ bitmap_prepend(bitmap_type * a, uint bits_per_field, uint value) {
 
 
 static inline uint
-bitmap_extract(bitmap_type * a, uint bits_per_field, uint index) {
+bitmap_extract(const bitmap_type * a, uint bits_per_field, uint index) {
   assert(index < (8 * sizeof(bitmap_type)) / bits_per_field);
 
   return (*a >> bits_per_field * index) & bitmap_all1_field(bits_per_field);
@@ -81,5 +81,48 @@ bitmap32_extract(uint32_t * a, uint bits_per_field, uint index) {
   return (*a >> bits_per_field * index) & bitmap32_all1_field(bits_per_field);
 }
 
+/* Dealing with longer bitmaps */
+
+static inline void
+bitmap_long_insert_clean(bitmap_type * a, uint bits_per_field, uint max_size, uint index, uint value) {
+  uint bitmap_part_size = sizeof(bitmap_type) * 8 / bits_per_field;
+  assert(index < (bitmap_part_size * max_size));
+  assert((value & ~bitmap_all1_field(bits_per_field)) == 0);
+
+  a[index/bitmap_part_size] |= ((bitmap_type)value << ((index % bitmap_part_size) * bits_per_field));
+}
+
+
+static inline void
+bitmap_long_clear(bitmap_type * a, uint bits_per_field, uint max_size, uint index) {
+  uint bitmap_part_size = sizeof(bitmap_type) * 8 / bits_per_field;
+  assert(index < (bitmap_part_size * max_size));
+
+  a[index/bitmap_part_size] &= (~(bitmap_all1_field(bits_per_field) << (index % bitmap_part_size) * bits_per_field));
+}
+
+
+static inline uint
+bitmap_long_extract(const bitmap_type * a, uint bits_per_field, uint max_size, uint index) {
+  uint bitmap_part_size = sizeof(bitmap_type) * 8 / bits_per_field;
+  assert(index < (bitmap_part_size * max_size));
+
+  return (a[index/bitmap_part_size] >> bits_per_field * (index % bitmap_part_size)) & bitmap_all1_field(bits_per_field);
+}
+
+static inline void
+bitmap_long_reset(bitmap_type * a, uint max_size) {
+  uint i;
+  for (i = 0; i < max_size; ++i) {
+	  a[i] = 0x0;
+  }
+}
+static inline void
+bitmap_long_allset(bitmap_type * a, uint max_size) {
+  uint i;
+  for (i = 0; i < max_size; ++i) {
+	  a[i] = ~(bitmap_type)0x0;
+  }
+}
 
 #endif
